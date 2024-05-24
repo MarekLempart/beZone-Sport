03c9cd884a18c30de63eda777c465acd92c936ed
MarekLempart <kama.zakupy@gmail.com>
2024-05-24 15:22:43 +0200

    Prace wstępne nad projektem 02

---
+++ src/js/index-ImagesImport.js
@@ -0,0 +1,19 @@
+// index-ImagesImport.js
+
+import dominikPhoto from '../images/reviews-Dominik.png';
+import edytaPhoto from '../images/reviews-Edyta.png';
+import ewelinaPhoto from '../images/reviews-Ewelina.png';
+import marekPhoto from '../images/reviews-Marek.webp';
+import nataliaPhoto from '../images/reviews-Natalia.png';
+import valeriiaPhoto from '../images/reviews-Valeriia.png';
+
+const images = {
+  dominikPhoto,
+  edytaPhoto,
+  ewelinaPhoto,
+  marekPhoto,
+  nataliaPhoto,
+  valeriiaPhoto,
+};
+
+export default images;
