bbc8201255e47c865e815ca73d02ab48393fe07e
MarekLempart <kama.zakupy@gmail.com>
2024-06-26 10:26:22 +0200

    poprawki Methodology

--- src/js/fakeAPI-Methodology.js
+++ src/js/fakeAPI-Methodology.js
@@ -1,45 +1,46 @@
 // fakeAPI-Methodology.js
 
 export const methodologyData = [
-  {
-    id: 'powiezi',
-    title: 'Rozluźnianie powięziowe',
-    text: `Twoje ciało sztywniejąc wytwarza coś w rodzaju zamarzniętych węzłów na powięzi dlatego zamiast rozciągania lepszym wyborem jestem praca bezpośrednio z tkanką zblokowaną w ten sposób dajesz ciału informacje tam gdzie trzeba zamiast strzelać na oślep. Dostaniesz techniki samorozluźniania, tak abyś wiedział jak pomóc sobie sam, aby lepiej się regenerować i poradzić sobie z nagłym spięciem. Technika masażu, która rozbija tkankę bliznowatą, jednocześnie nawadniając mięśnie. Jest to skuteczny sposób na uwolnienie napięcia, stopniowo pozwalając ciału poruszać się z mniejszymi ograniczeniami.`,
-  },
-  {
-    id: 'oddech',
-    title: 'Praca z oddechem',
-    text: `Odpowiedni wzorzec oddechu jest kluczowy dla silnego odpornego na stres organizmu. Jednak jeśli twoje ciało nie wie jak poprowadzić oddech to same ćwiczenia oddechowe nie wystarczą. Pracując z oddechem intencjonalnie, nauczymy Cię jak go kierować jak najefektywniej tak by pozwał on stabilizację twojego tułowia podczas ruchu.`,
-  },
+  // {
+  //   id: 'powiezi',
+  //   title: 'Rozluźnianie powięziowe',
+  //   text: `Twoje ciało sztywniejąc wytwarza coś w rodzaju zamarzniętych węzłów na powięzi dlatego zamiast rozciągania lepszym wyborem jestem praca bezpośrednio z tkanką zblokowaną w ten sposób dajesz ciału informacje tam gdzie trzeba zamiast strzelać na oślep. Dostaniesz techniki samorozluźniania, tak abyś wiedział jak pomóc sobie sam, aby lepiej się regenerować i poradzić sobie z nagłym spięciem. Technika masażu, która rozbija tkankę bliznowatą, jednocześnie nawadniając mięśnie. Jest to skuteczny sposób na uwolnienie napięcia, stopniowo pozwalając ciału poruszać się z mniejszymi ograniczeniami.`,
+  // },
 
   {
-    id: 'sensoryczny',
-    title: 'Trening sensoryczny',
-    text: `Twój układ nerwowy jest szefem, a dla niego kluczowe są informacje zbierane z twojego ciała j otoczenia. Niektóre obszary w twoim ciele są zamrożone, poprzez styl życia, zebrane obciążenia, z którymi ciało nieporadziło sobie. Potrzebujemy przekonać twój układ nerwowy, ze jest tam bezpiecznie, dając mu odpowiednie odczucia tego miejsca. W ten sposób możesz zacząć odzyskiwać swoje ciało i siebie.`,
-  },
-
-  {
-    id: 'postawa',
+    id: 'attitude',
     title: 'Postawa',
     text: `Korekcja asymetrii poprzez ruch, budowanie lepszej świadomości twojej postawy buduje solidne fundamenty, które pomagają w pozbyciu się bólu i zapobiegają kontuzjom i poprawiają ogólną sprawność.`,
   },
 
   {
-    id: 'cwiczenia01',
-    title: 'Ćwiczenia korekcyjne',
-    text: `Twoje ciało posiada coś takiego jak mechanoreceptory. Znaczy takie czujki, które odczytują napięcie z mięśni. Ewolucyjnie utworzyły nam się faworyzowane ścieżki przekazywania tych napięć, bazujące na ruchach, które robiliśmy najczęściej. Jeśli nauczymy twoje ciało działać przez te ścieżki to twoje ciało będzie bezpieczniejsze i silniejsze w ruchu. Kontekstowe ćwiczenia w tych funkcji (stanie, chodzenie, bieganie, rzucanie) pomagają przywrócić twojemu ciału te wzorce ruchu.`,
+    id: 'sensor',
+    title: 'Trening sensoryczny',
+    text: `Twój układ nerwowy jest szefem, a dla niego kluczowe są informacje zbierane z twojego ciała j otoczenia. Niektóre obszary w twoim ciele są zamrożone, poprzez styl życia, zebrane obciążenia, z którymi ciało nieporadziło sobie. Potrzebujemy przekonać twój układ nerwowy, ze jest tam bezpiecznie, dając mu odpowiednie odczucia tego miejsca. W ten sposób możesz zacząć odzyskiwać swoje ciało i siebie.`,
   },
 
   {
-    id: 'cwiczenia02',
-    title: 'Ćwiczenia dynamiczne',
-    text: `Podczas treningu będziemy wykonywać dynamiczne ćwiczenia, które odwzorowują rzeczywiste sytuacje życiowe i sportowe. Będziesz poruszać się pewniej, mocniej i stabilnie.`,
+    id: 'breath',
+    title: 'Praca z oddechem',
+    text: `Odpowiedni wzorzec oddechu jest kluczowy dla silnego odpornego na stres organizmu. Jednak jeśli twoje ciało nie wie jak poprowadzić oddech to same ćwiczenia oddechowe nie wystarczą. Pracując z oddechem intencjonalnie, nauczymy Cię jak go kierować jak najefektywniej tak by pozwał on stabilizację twojego tułowia podczas ruchu.`,
   },
 
   {
     id: 'neuroFitness',
     title: 'Neuro-Fitness',
     text: `Neuro-Fitness obejmuje ruchy, które opierają się na poprawie naszych funkcji neurologicznych, takich jak wzrok, węch, słuch, pamięć i inne.`,
+  },
+
+  {
+    id: 'dynamic',
+    title: 'Ćwiczenia dynamiczne',
+    text: `Podczas treningu będziemy wykonywać dynamiczne ćwiczenia, które odwzorowują rzeczywiste sytuacje życiowe i sportowe. Będziesz poruszać się pewniej, mocniej i stabilnie.`,
+  },
+
+  {
+    id: 'correction',
+    title: 'Ćwiczenia korekcyjne',
+    text: `Twoje ciało posiada coś takiego jak mechanoreceptory. Znaczy takie czujki, które odczytują napięcie z mięśni. Ewolucyjnie utworzyły nam się faworyzowane ścieżki przekazywania tych napięć, bazujące na ruchach, które robiliśmy najczęściej. Jeśli nauczymy twoje ciało działać przez te ścieżki to twoje ciało będzie bezpieczniejsze i silniejsze w ruchu. Kontekstowe ćwiczenia w tych funkcji (stanie, chodzenie, bieganie, rzucanie) pomagają przywrócić twojemu ciału te wzorce ruchu.`,
   },
 
   // Dodaj resztę danych w podobnym formacie...
--- src/js/index-ImagesImport-Methodology.js
+++ src/js/index-ImagesImport-Methodology.js
@@ -1,21 +1,21 @@
 // index-ImagesImport-Methodology.js
 
 // photos for Methodology
-import imgPostawa from '../images/ico_attitude.png';
-import imgOddech from '../images/ico_breath.png';
-import imgCwiczenia01 from '../images/ico_correction.png';
-import imgCwiczenia02 from '../images/ico_dynamic.png';
+import imgAttitude from '../images/ico_attitude.png';
+import imgBreath from '../images/ico_breath.png';
+import imgCorrection from '../images/ico_correction.png';
+import imgDynamic from '../images/ico_dynamic.png';
 import imgNeuroFitness from '../images/ico_neurofitness.png';
-import imgSensoryczny from '../images/ico_sensor.png';
+import imgSensor from '../images/ico_sensor.png';
 import imgPowiezi from '../images/workingMethodology-Powiezi.webp';
 
 const imagesMethodology = [
   { id: 'powiezi', src: imgPowiezi },
-  { id: 'oddech', src: imgOddech },
-  { id: 'sensoryczny', src: imgSensoryczny },
-  { id: 'postawa', src: imgPostawa },
-  { id: 'cwiczenia01', src: imgCwiczenia01 },
-  { id: 'cwiczenia02', src: imgCwiczenia02 },
+  { id: 'breath', src: imgBreath },
+  { id: 'sensor', src: imgSensor },
+  { id: 'attitude', src: imgAttitude },
+  { id: 'correction', src: imgCorrection },
+  { id: 'dynamic', src: imgDynamic },
   { id: 'neuroFitness', src: imgNeuroFitness },
 ];
 
