b77a95ad7d66da5acc1c76541fe8bf24803c9a1b
MarekLempart <kama.zakupy@gmail.com>
2024-07-07 15:57:11 +0200

    Dalsze prace nad MSystem

--- src/js/index-ImagesImport-MSystem.js
+++ src/js/index-ImagesImport-MSystem.js
@@ -6,7 +6,7 @@ import imgFunctionalPatterns from '../images/System-functionalPatterns.webp';
 import imgMotionData from '../images/System-motionData.webp';
 import imgMovMed from '../images/System-movMed.webp';
 
-const imagesMethodology = [
+const imagesMethodologySystem = [
   { id: 'functionalPatterns', src: imgFunctionalPatterns },
   { id: 'movMed', src: imgMovMed },
   { id: 'motionData', src: imgMotionData },
