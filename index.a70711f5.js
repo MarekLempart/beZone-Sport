09ef433d9b0c9be479b3aae8f460cedeabf3ed3d
MarekLempart <144551338+MarekLempart@users.noreply.github.com>
2024-09-04 11:45:27 +0000

    Deploying to gh-pages from @ MarekLempart/beZone-Sport@dbd97384c2a916bd1f35bc90b39f884421a68508 🚀

---
+++ index.a70711f5.js
@@ -0,0 +1,2 @@
+var scrollToTopButton=document.getElementById("scrollToTopButton");window.addEventListener("scroll",(function(){window.pageYOffset>100?scrollToTopButton.style.display="block":scrollToTopButton.style.display="none"})),scrollToTopButton.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.a70711f5.js.map
