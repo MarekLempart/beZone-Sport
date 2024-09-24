02e799774e0b6cfa9f22b8bfa3da3c960d5b3618
Marek Lempart <kama.zakupy@gmail.com>
2024-09-24 15:06:37 +0200

    lazyloaded fix

--- src/js/lazyloaded.js
+++ src/js/lazyloaded.js
@@ -9,8 +9,8 @@ import image4 from '../images/forWhom_firstPhoto_05.webp';
 import image5 from '../images/howWork_07.webp';
 import image6 from '../images/myPhoto_tablet.webp';
 import image1 from '../images/preliminary-first-container_01.webp';
-import image2 from '../images/timeToChange-img_03.webp';
-import image3 from '../images/timeToChange-img_04.webp';
+import image2 from '../images/timeToChange1-img_03.webp';
+import image3 from '../images/timeToChange2-img_04.webp';
 
 // Importuj obrazy tła
 import desktopBg from '../images/methodology-footer_background_desktop_08.webp';
@@ -21,32 +21,69 @@ document.addEventListener('DOMContentLoaded', function () {
   // Pobieramy wszystkie obrazy lazyload i dynamicznie ustawiamy ścieżki do plików
   const lazyImages = document.querySelectorAll('.lazyload');
 
+  const imagesObject = {
+    preliminary: image1,
+    timeToChange1: image2,
+    timeToChange2: image3,
+    forWhom: image4,
+    howWork: image5,
+    myPhoto: image6,
+  };
+
   lazyImages.forEach(img => {
+    const partialString = img.getAttribute('data-src').slice(9);
+    const key = partialString.substring(0, partialString.indexOf('-'));
+    img.setAttribute('data-src', imagesObject[key]);
+
+    // lazyImages.forEach(img => {
+    //   switch(img.getAttribute('data-src')) {
+    //     case './images/preliminary-first-container_01.webp':
+    //       img.setAttribute('data-src', image1);
+    //       break;
+    //     case './images/timeToChange-img_03.webp':
+    //       img.setAttribute('data-src', image2);
+    //       break;
+    //     case './images/timeToChange-img_04.webp':
+    //       img.setAttribute('data-src', image3);
+    //       break;
+    //     case './images/forWhom_firstPhoto_05.webp':
+    //       img.setAttribute('data-src', image4);
+    //       break;
+    //     case './images/howWork_07.webp':
+    //       img.setAttribute('data-src', image5);
+    //       break;
+    //     case './images/myPhoto_tablet.webp':
+    //       img.setAttribute('data-src', image6);
+    //       break;
+    //     default:
+    //       break;
+    //   }
+
     // Jeśli nie masz problemów z Parcel, to te linie nie są potrzebne.
-    if (
-      img.getAttribute('data-src') ===
-      './images/preliminary-first-container_01.webp'
-    ) {
-      img.setAttribute('data-src', image1);
-    } else if (
-      img.getAttribute('data-src') === './images/timeToChange-img_03.webp'
-    ) {
-      img.setAttribute('data-src', image2);
-    } else if (
-      img.getAttribute('data-src') === './images/timeToChange-img_04.webp'
-    ) {
-      img.setAttribute('data-src', image3);
-    } else if (
-      img.getAttribute('data-src') === './images/forWhom_firstPhoto_05.webp'
-    ) {
-      img.setAttribute('data-src', image4);
-    } else if (img.getAttribute('data-src') === './images/howWork_07.webp') {
-      img.setAttribute('data-src', image5);
-    } else if (
-      img.getAttribute('data-src') === './images/myPhoto_tablet.webp'
-    ) {
-      img.setAttribute('data-src', image6);
-    }
+    // if (
+    //   img.getAttribute('data-src') ===
+    //   './images/preliminary-first-container_01.webp'
+    // ) {
+    //   img.setAttribute('data-src', image1);
+    // } else if (
+    //   img.getAttribute('data-src') === './images/timeToChange-img_03.webp'
+    // ) {
+    //   img.setAttribute('data-src', image2);
+    // } else if (
+    //   img.getAttribute('data-src') === './images/timeToChange-img_04.webp'
+    // ) {
+    //   img.setAttribute('data-src', image3);
+    // } else if (
+    //   img.getAttribute('data-src') === './images/forWhom_firstPhoto_05.webp'
+    // ) {
+    //   img.setAttribute('data-src', image4);
+    // } else if (img.getAttribute('data-src') === './images/howWork_07.webp') {
+    //   img.setAttribute('data-src', image5);
+    // } else if (
+    //   img.getAttribute('data-src') === './images/myPhoto_tablet.webp'
+    // ) {
+    //   img.setAttribute('data-src', image6);
+    // }
   });
 
   // Ustawiamy tło dla sekcji metodologii w zależności od rozmiaru okna
