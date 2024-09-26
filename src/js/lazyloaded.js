e8ebb8efbc4e5c06f5b0f26f6f92d1067592a1d4
Marek Lempart <kama.zakupy@gmail.com>
2024-09-26 13:42:47 +0200

    Update lazyloaded.js

--- src/js/lazyloaded.js
+++ src/js/lazyloaded.js
@@ -17,75 +17,72 @@ import desktopBg from '../images/methodology-footer_background_desktop_08.webp';
 import mobileBg from '../images/methodology-footer_background_mobile_08.webp';
 import tabletBg from '../images/methodology-footer_background_tablet_08.webp';
 
+// Tworzymy obiekt z kluczami i odpowiadającymi im obrazami
+const imagesObject = {
+  preliminary: image1,
+  timeToChange01: image2,
+  timeToChange02: image3,
+  forWhom: image4,
+  howWork: image5,
+  aboutUs: image6,
+};
+
+// Funkcja wykonująca operacje po załadowaniu DOM
 document.addEventListener('DOMContentLoaded', function () {
-  // Pobieramy wszystkie obrazy lazyload i dynamicznie ustawiamy ścieżki do plików
+  // Pobieramy wszystkie obrazy lazyload
   const lazyImages = document.querySelectorAll('.lazyload');
 
-  // const imagesObject = {
-  //   preliminary: image1,
-  //   timeToChange1: image2,
-  //   timeToChange2: image3,
-  //   forWhom: image4,
-  //   howWork: image5,
-  //   myPhoto: image6,
-  // };
-
-  // lazyImages.forEach(img => {
-  //   const partialString = img.getAttribute('data-src').slice(9);
-  //   const key = partialString.substring(0, partialString.indexOf('_'));
-  //   img.setAttribute('data-src', imagesObject[key]);
-
-  // lazyImages.forEach(img => {
-  //   switch(img.getAttribute('data-src')) {
-  //     case './images/preliminary-first-container_01.webp':
-  //       img.setAttribute('data-src', image1);
-  //       break;
-  //     case './images/timeToChange-img_03.webp':
-  //       img.setAttribute('data-src', image2);
-  //       break;
-  //     case './images/timeToChange-img_04.webp':
-  //       img.setAttribute('data-src', image3);
-  //       break;
-  //     case './images/forWhom_firstPhoto_05.webp':
-  //       img.setAttribute('data-src', image4);
-  //       break;
-  //     case './images/howWork_07.webp':
-  //       img.setAttribute('data-src', image5);
-  //       break;
-  //     case './images/myPhoto_tablet.webp':
-  //       img.setAttribute('data-src', image6);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-
-  // Jeśli nie masz problemów z Parcel, to te linie nie są potrzebne.
+  // Pętla przechodząca po każdym obrazie
   lazyImages.forEach(img => {
-    if (img.getAttribute('data-src') === './images/preliminary-img_01.webp') {
-      img.setAttribute('data-src', image1);
-    } else if (
-      img.getAttribute('data-src') === './images/timeToChange-img_03.webp'
-    ) {
-      img.setAttribute('data-src', image2);
-    } else if (
-      img.getAttribute('data-src') === './images/timeToChange-img_04.webp'
-    ) {
-      img.setAttribute('data-src', image3);
-    } else if (
-      img.getAttribute('data-src') === './images/forWhom-img_05.webp'
-    ) {
-      img.setAttribute('data-src', image4);
-    } else if (
-      img.getAttribute('data-src') === './images/howWork-img_07.webp'
-    ) {
-      img.setAttribute('data-src', image5);
-    } else if (
-      img.getAttribute('data-src') === './images/aboutUs-img_12.webp'
-    ) {
-      img.setAttribute('data-src', image6);
+    // Sprawdzamy, czy atrybut 'data-src' istnieje
+    const dataSrc = img.getAttribute('data-src');
+
+    if (dataSrc) {
+      // Pobieramy nazwę pliku z atrybutu 'data-src', zaczynając od 9 znaku (omijając './images/')
+      const partialString = dataSrc.slice(9);
+
+      // Wyciągamy klucz (część nazwy pliku przed '-img')
+      const key = partialString.substring(0, partialString.indexOf('-img'));
+
+      // Jeśli klucz istnieje w obiekcie imagesObject, ustawiamy nową wartość dla 'data-src'
+      if (imagesObject[key]) {
+        img.setAttribute('data-src', imagesObject[key]);
+      }
     }
   });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Pobieramy wszystkie obrazy lazyload i dynamicznie ustawiamy ścieżki do plików
+//   const lazyImages = document.querySelectorAll('.lazyload');
+
+//   // Jeśli nie masz problemów z Parcel, to te linie nie są potrzebne.
+//   lazyImages.forEach(img => {
+//     if (img.getAttribute('data-src') === './images/preliminary-img_01.webp') {
+//       img.setAttribute('data-src', image1);
+//     } else if (
+//       img.getAttribute('data-src') === './images/timeToChange-img_03.webp'
+//     ) {
+//       img.setAttribute('data-src', image2);
+//     } else if (
+//       img.getAttribute('data-src') === './images/timeToChange-img_04.webp'
+//     ) {
+//       img.setAttribute('data-src', image3);
+//     } else if (
+//       img.getAttribute('data-src') === './images/forWhom-img_05.webp'
+//     ) {
+//       img.setAttribute('data-src', image4);
+//     } else if (
+//       img.getAttribute('data-src') === './images/howWork-img_07.webp'
+//     ) {
+//       img.setAttribute('data-src', image5);
+//     } else if (
+//       img.getAttribute('data-src') === './images/aboutUs-img_12.webp'
+//     ) {
+//       img.setAttribute('data-src', image6);
+//     }
+//   });
+// });
 
 // Ustawiamy tło dla sekcji metodologii w zależności od rozmiaru okna
 const methodologyBgContainer = document.querySelector(
